390fa90fbb7b9ef4d67d06a9667b9bb022f5de32
unknownpgr <unknownpgr@gmail.com>
2020-08-11 11:59:33 +0900

    Implement live streaming

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-# node_webcam_streaming
-## Node+FFmepg webcam live streaming server / client
-### How to use
-- Download index.js(server) and index.html(client)
-- Execute server with nodejs(execute with superuser for getting video permission)
-- Connect to localhost:1415 with chrome
-### Server structure
-- Record from webcam (/dev/video0)
-- Convert stream codec from mjpeg to webm with ffmpeg
-- Split stream into chunk files with nodejs
-- Send chunk files to client
-### Client structre
-- Create mediaSource and link it to video tag
-- Request chunk files from server
-- Feed chunk files to mediaSource
-### Dependency
-- express
-- ffmpeg
+# Node / FFmpeg Webcam Streaming
+
+- Streaming the webcam video to html with HLS(HTTP Live Streaming) format.
+- It is Node.js wrapper of FFmpeg.
+- It works on linux. (uses v4l2)
+
+## How it works
+
+- When the server starts, it execute FFmpeg.
+- FFmpeg takes video from webcam and converts it to HLS format.
+- The .m3u8 files and video segments are (statically) served by Node.js server.
+- Now you can watch live video from HTTP page. The video will have up to 30 second delay.
+- See [server](./index.js) and [html](./public/index.html) for better understand. It is very simple!
+
+## How to run
+
+1. It requires Node.js and FFmpeg. You can download FFmpeg [here](https://ffmpeg.org/download.html).
+2. To initialize server, run `yarn install` or `npm install`
+3. To start the server, run `node index.js`
+4. Connect to localhost:3000 with a web browser.
+
+## Screenshot
+
+![screenshot](screenshot.png)
--- index.js
+++ index.js
@@ -1,34 +1,57 @@
 const express = require("express");
-const { spawn } = require("child_process");
-
+const { spawn, exec, execSync } = require("child_process");
+const { message } = require("statuses");
+const fs = require("fs").promises;
 const app = express();
+
+const CAMERA = "/dev/video0";
 
 let ffmpegArgs = [
   "-f", // format = v4l2
   "v4l2",
-  "-s",
-  "640x480",
-  "-i", // input from video device
-  "/dev/video0",
-  "-c:v",
-  "h264",
-  "-crf",
-  "22",
-  "-hls_time", // hls segment time=8
-  "8",
-  //   "-hls_list_size", // set maximum list size = 3
-  //   "3",
-  //   "-hls_delete_threshold", // set maximum segment number = hls_list_size+hls_delete_threshold = 4
-  //   "1",
-  //   "-hls_flags", // remove outdated segments
-  //   "delete_segments",
+  "-i", // input = /dev/video0
+  CAMERA,
+  "-c:v", // video codec = libx264
+  "libx264",
+  "-crf", // constant rate factor = 23(default)
+  "23",
+  "-pix_fmt", // Required for compatibility
+  "yuv420p",
+  "-hls_time", // hls segment length
+  "2",
+  "-hls_list_size", // set maximum list size = 3
+  "5",
+  "-hls_delete_threshold", // set maximum segment number = hls_list_size+hls_delete_threshold = 4
+  "1",
+  "-hls_flags", // remove outdated segments
+  "delete_segments",
   "-f", // output format = hls
   "hls",
   "public/video.m3u8", // output file
 ];
 
-spawn("ffmpeg", ffmpegArgs);
+async function main() {
+  console.log("Set permission of camera device ");
+  console.log(execSync(`sudo chmod 777 ${CAMERA}`).toString());
+  console.log("Initialize video");
+  console.log(execSync(`rm -f ${__dirname}/public/*.ts`).toString());
+  console.log(execSync(`rm -f ${__dirname}/public/*.m3u8`).toString());
+  console.log("Start streaming");
 
+  let argStr = "";
+  ffmpegArgs.forEach((arg) => (argStr += arg + " "));
+  console.log("Run : ffmpeg " + argStr);
+
+  let ffmpeg = spawn("ffmpeg", ffmpegArgs);
+  ffmpeg.on("exit", () => console.log("ffmpeg finished"));
+  ffmpeg.on("error", console.error);
+  ffmpeg.on("disconnect", () => console.log("disconnect"));
+  ffmpeg.on("message", () => console.log("message"));
+}
+
+main();
+
+// Set static directory
 app.use(express.static(__dirname + "/public"));
 
 // Index page
